380a447f9ce3d8ec785c7ed87cd02f3bab281f0c
Metalik <1781573+MetalikZX@users.noreply.github.com>
2025-11-28 01:26:34 +0300

    Added basic functionality with dynamic resizing

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container');
+let numberOfSquares = 16;
+
+
+function createGrid(size) {
+    console.log("Creating grid of size:", size);
+    container.innerHTML = '';
+    const widthOrHeight = `${100 / size}%`;
+for (let i = 0; i < (size * size); i++) {
+            const grid = document.createElement('div');
+            grid.classList.add('grid-style');
+            container.appendChild(grid);
+            grid.style.width = widthOrHeight;
+            grid.style.height = widthOrHeight;
+            };}
+
+            container.addEventListener("mouseover", (event) => {
+                if (event.target.classList.contains('grid-style')) {
+                event.target.style.backgroundColor = 'black';
+            }})
+
+            const sizeSlider = document.querySelector('#sizeSlider')
+            const sizeText = document.querySelector('#sizeText');
+            sizeSlider.addEventListener('input', () => {
+                const newSize = sizeSlider.value
+            sizeText.textContent = `${sizeSlider.value}x${sizeSlider.value}`;
+            createGrid(newSize);
+            });
